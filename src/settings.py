c359a175c2d0c2ba754fb6ade55f2086ba8dc270
Your Name <you@example.com>
2023-05-01 14:32:59 +0500

    nmadr

--- src/settings.py
+++ src/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#28t5kn46y^gw718a#*1n@xl_tl^(@o^afst40l(vu4ky57js9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
